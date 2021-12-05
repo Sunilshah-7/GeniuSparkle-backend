c345873adc4697d6873bdb74f959c9768f8fa9b8
Sunilshah-7 <sunilshah2416@gmail.com>
2021-12-05 20:37:11 +0545

    Port added

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
-
-
-server.listen(3000, function () {
-  console.log("server is listening on port: 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function () {
+  console.log("server is listening on port: " + PORT);
 });
