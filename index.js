a5185da51499d306bdecb6b8dae61cc905fd770b
Sunilshah-7 <sunilshah2416@gmail.com>
2021-12-28 21:01:24 +0545

    Commented the admin for testing

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const resetPasswordRouter = require("./routes/reset_password");
 const settingRouter = require("./routes/settings");
 const petCheckoutRouter = require("./routes/petCheckoutDetails");
 const userDetailsRouter = require("./routes/userDetails");
-const adminRouter = require("./routes/admin");
+// const adminRouter = require("./routes/admin");
 // const questionRouter = require('./routes/questions');
 
 const app = express();
@@ -31,7 +31,6 @@ mongoose
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-
 //routes
 app.use("/api/auth/login", loginRouter);
 app.use("/api/auth/signup", registerRouter);
@@ -39,15 +38,14 @@ app.use("/api/user/reset_password", resetPasswordRouter);
 app.use("/api/user/settings", settingRouter);
 app.use("/api/user/checkout/pet_details", petCheckoutRouter);
 app.use("/api/user/user_details", userDetailsRouter);
-app.use("/api/admin", adminRouter);
+// app.use("/api/admin", adminRouter);
 // app.use("/questions", questionRouter);
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./public/index.html"));
 // });
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, function () {
   console.log("server is listening on port: " + PORT);
 });
